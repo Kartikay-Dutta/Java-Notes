277a3c3fd9dc6e2438e843989a29220fe1f7674d
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-08 12:32:27 +0530

    if else code

---
+++ src/If_else.java
@@ -0,0 +1,9 @@
+public class If_else {
+    public static void main(String[] args) {
+        int marks = 90;
+
+        if (marks >= 90) {
+            System.out.println("Excellent");
+        }
+    }
+}
