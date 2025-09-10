00bf75b833b2edd4dda72da054b3979faf081052
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-10 11:55:41 +0530

    code

--- src/Identifiers.java
+++ src/Identifiers.java
@@ -1,16 +1,16 @@
-public class Identifiers {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-
-        int sum = a + b;
-        int diff = a - b;
-        int div = a / b;
-        int mul = a * b;
-
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + diff);
-        System.out.println("Division: " + div);
-        System.out.println("Multiplication: " + mul);
-    }
-}
+//public class Identifiers {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 5;
+//
+//        int sum = a + b;
+//        int diff = a - b;
+//        int div = a / b;
+//        int mul = a * b;
+//
+//        System.out.println("Sum: " + sum);
+//        System.out.println("Difference: " + diff);
+//        System.out.println("Division: " + div);
+//        System.out.println("Multiplication: " + mul);
+//    }
+//}
--- src/If_else.java
+++ src/If_else.java
@@ -1,9 +1,9 @@
-public class If_else {
-    public static void main(String[] args) {
-        int marks = 90;
-
-        if (marks >= 90) {
-            System.out.println("Excellent");
-        }
-    }
-}
+//public class If_else {
+//    public static void main(String[] args) {
+//        int marks = 90;
+//
+//        if (marks >= 90) {
+//            System.out.println("Excellent");
+//        }
+//    }
+//}
