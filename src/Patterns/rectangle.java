dc9ff9fc10b7d99c7216ce940fe6c74040ded332
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-11 11:21:26 +0530

    rectangle

---
+++ src/Patterns/rectangle.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class rectangle {
+    public static void main(String[] args){
+        int l = 4;
+        int b = 5;
+        for(int i = 0;i<l;i++){
+            for(int j = 0;j<b;j++){
+                System.out.print('*');
+
+            }
+            System.out.println();
+        }
+    }
+}
