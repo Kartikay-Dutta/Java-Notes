aadd31906f6ed908db690b371e755cae14e4f1c6
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-15 11:37:41 +0530

    code

---
+++ src/Arrays/basic_array.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class basic_array {
+    public static void main(String[] args){
+        int[] numbers = new int[5]; //declaration with size 5
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+        // or direct initialization
+        int[] scores = {85,90,78,92,88};
+    }
+}
---
+++ src/Arrays/traversing_arr.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.sql.SQLOutput;
+
+public class traversing_arr {
+    public static void main(String[] args){
+        int[] arr = {4,7,1,9,3};
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Element at index " +i + ": "+ arr[i]);
+        }
+    }
+}
