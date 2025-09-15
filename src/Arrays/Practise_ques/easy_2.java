3eac59f9352e6bb82514c94d6759cf68ace90946
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-15 12:09:33 +0530

    code

---
+++ src/Arrays/Practise_ques/easy_2.java
@@ -0,0 +1,13 @@
+package Arrays.Practise_ques;
+
+public class easy_2 {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        int sum = 0;
+        for(int i =0;i<arr.length;i++){
+            sum = sum+arr[i];
+
+        }
+        System.out.println(sum);
+    }
+}
