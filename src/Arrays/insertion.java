ca036f74f1686da279b15ea3e26a9c26320ee323
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-15 11:45:51 +0530

    code

--- src/Arrays/insertion.java
+++ src/Arrays/insertion.java
@@ -1,4 +1,22 @@
 package Arrays;
 
+import java.util.Arrays; // Needed for Arrays.toString()
+
 public class insertion {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 0}; // size 5, last space free
+        int n = 4;  // Current number of elements
+        int value = 25;
+        int pos = 2; // Insert at index 2 (0-based)
+
+        for (int i = n; i > pos; i--) {
+            arr[i] = arr[i - 1]; // Shift elements to the right
+        }
+
+        arr[pos] = value; // Insert value
+        n++;
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
+
