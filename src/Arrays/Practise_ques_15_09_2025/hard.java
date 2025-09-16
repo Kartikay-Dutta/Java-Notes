c9036285f2e29f6b3626e00f3b94877841dd5bd5
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-16 11:26:30 +0530

    code

---
+++ src/Arrays/Practise_ques_15_09_2025/hard.java
@@ -0,0 +1,7 @@
+package Arrays.Practise_ques_15_09_2025;
+
+public class hard {
+    public static void main(String[] args){
+          
+    }
+}
---
+++ src/Arrays/basic_array_2D.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class basic_array_2D {
+    public static void main(String[] args){
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+    }
+}
