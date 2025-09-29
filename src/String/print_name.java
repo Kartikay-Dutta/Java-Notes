2d8679a5e803ce2a60ad343354c4869d8b405118
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-09-29 11:36:41 +0530

    code

---
+++ src/String/print_name.java
@@ -0,0 +1,10 @@
+package String;
+
+public class print_name {
+    public static void main(String[] args) {
+        char[] name  ={'K','a','r','t','i','k','a','y'};
+        for(int i = 0;i<name.length;i++){
+            System.out.print(name[i]+ " ");
+        }
+    }
+}
