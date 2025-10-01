80fb09147924e0e11e2a9d4146a01359b1f44f66
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-10-01 11:35:39 +0530

    code

---
+++ src/String/string_buffer.java
@@ -0,0 +1,13 @@
+package String;
+
+public class string_buffer {
+    public static void main(String[] args) {
+        String str = "Kartikay";
+        String upper_str = str.toUpperCase();
+        System.out.println(str);
+
+        char index_char = str.charAt(1);
+        System.out.println(index_char);
+
+    }
+}
