711f937266fdc154248c3ee30543f851e2172053
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-10-06 12:03:46 +0530

    code

---
+++ src/String/interface_.java
@@ -0,0 +1,11 @@
+package String;
+
+public class interface_ {
+    public static void main(String[] args) {
+        interface Swimmable
+        {
+            void swim(); // Abstract method(implicitly)
+            int MAX_DEPTH = 100;
+        }
+    }
+}
