8d405771996579d719e649e0ff30c198dba06353
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-10-08 22:46:52 +0530

    cod

---
+++ src/Constructor/copy_constructor.java
@@ -0,0 +1,13 @@
+package Constructor;
+
+public class copy_constructor {
+    public static void main(String[] args) {
+        class Student{
+            private String name;
+            private int age;
+
+            // Regular constructor
+
+        }
+    }
+}
