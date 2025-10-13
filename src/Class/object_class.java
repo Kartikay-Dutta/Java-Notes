14656b270d87c48463641b2d2830f39da5cb3bfd
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-10-13 11:05:41 +0530

    code

---
+++ src/Class/object_class.java
@@ -0,0 +1,13 @@
+//package Class;
+//public class Student{
+//    private String name;
+//
+//        public Student(String name){
+//            this.name = name;
+//        }
+//}
+//public class object_class {
+//    public static void main(String[] args) {
+//        Student student = new Student("John");
+//    }
+//}
