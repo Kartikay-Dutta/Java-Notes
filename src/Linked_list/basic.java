62f0590fa2804b1c61fdb642db6a1470e86a7de3
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-10-31 12:12:10 +0530

    code

--- src/Linked_list/basic.java
+++ src/Linked_list/basic.java
@@ -2,7 +2,7 @@ package Linked_list;
 
 public class basic {
 
-    static class Node {  // make Node static so it can be used in main
+    static class Node {
         int data;
         Node next;
 
@@ -12,21 +12,36 @@ public class basic {
         }
     }
 
+    // Method to traverse the linked list
     void traverse(Node head) {
         Node current = head;
         while (current != null) {
             System.out.print(current.data + " ");
             current = current.next;
         }
+        System.out.println();
+    }
+
+    // Method to insert a node at the beginning
+    Node insertAtBeginning(Node head, int value) {
+        Node newNode = new Node(value);
+        newNode.next = head;
+        return newNode;
     }
 
     public static void main(String[] args) {
-        basic list = new basic();  // create an object to call traverse()
+        basic list = new basic();  // create an object to call methods
 
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
 
-        list.traverse(head);  // call the traverse method
+        System.out.print("Original list: ");
+        list.traverse(head);
+
+        head = list.insertAtBeginning(head, 0); // insert 0 at the beginning
+
+        System.out.print("After insertion: ");
+        list.traverse(head);
     }
 }
