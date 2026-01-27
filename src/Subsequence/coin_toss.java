4b820dca5bd38da45c658150f60714037ada7a34
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-01-27 11:52:07 +0530

    cointoss

---
+++ src/Subsequence/coin_toss.java
@@ -0,0 +1,18 @@
+package Subsequence;
+
+public class coin_toss {
+    static void coinToss(int n, String ans){
+        if(n==0){
+            System.out.println(ans);
+            return;
+        }
+        coinToss(n-1, ans+"H");
+        coinToss(n-1, ans + "T");
+
+
+    }
+
+    public static void main(String[] args) {
+        coinToss(2, "");
+    }
+}
