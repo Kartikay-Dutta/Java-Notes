e590a555288f82fe2fbd136948b2d2b85d5e8f71
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-02-04 11:38:37 +0530

    quick_sort

--- src/Sorting/quick_sort.java
+++ src/Sorting/quick_sort.java
@@ -1,7 +1,33 @@
 package Sorting;
 
 public class quick_sort {
+    static void qs(int[] arr, int low, int high){
+        if(low<high){
+            int p = partition(arr,low,high);
+            qs(arr,low,p-1);
+            qs(arr,p+1,high);
+        }
+    }
+    static int partition(int [] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+
+        for(int j = low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i]= arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
     public static void main(String[] args) {
 
     }
-}
+  }
