eb5b9051e3dd757bb4624fe3f0c1b25af6e9997f
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-02-10 11:40:01 +0530

    code

---
+++ src/Exception_handling/basic.java
@@ -0,0 +1,18 @@
+package Exception_handling;
+
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a / b;
+            System.out.println("Result = "+ result);
+        }
+        catch(Exception e){
+            System.out.println("Wrong value entered. Enter Again");
+        }
+    }
+}
