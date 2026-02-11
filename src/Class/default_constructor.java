576b0e612bea1cdae841952eae7089b181e8a8ad
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-02-11 19:09:04 +0530

    code

---
+++ src/Class/default_constructor.java
@@ -0,0 +1,14 @@
+package Class;
+
+public class default_constructor {
+
+    static class Test {
+        Test() {
+            System.out.println("Constructor called");
+        }
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+    }
+}
