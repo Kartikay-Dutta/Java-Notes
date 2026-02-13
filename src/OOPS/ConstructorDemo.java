1a3faebd497513eafb07e0fae4808887f3f9af66
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-02-13 11:43:23 +0530

    constructor

--- src/OOPS/ConstructorDemo.java
+++ src/OOPS/ConstructorDemo.java
@@ -1,11 +1,24 @@
 package OOPS;
 
-class ConstructorDemo {
+class ConstructorDemo { //isko public nahi kar skte coz dusri files isko filename se access karengi.
     int x; //jvm default zero deta hai under default constructor.
     ConstructorDemo(){
+//      int y; // apne aap initialize nahi hoga so itll give an error\
+        int x = 20;
+        this.x = x; // this is used to access the object
+        int y = 10;
         System.out.println(x);
+        System.out.println(y);
+    }
+    ConstructorDemo(int x, int y){ // constructor overloading: same constructor with diff parameters
+        System.out.println("In parameterized constructor.");
+        this.fun(); // this ke saath methods bhi access hota hai not only objects
+    }
+    void fun(){
+        System.out.println("In fun method");
     }
     public static void main(String[] args) {
-        ConstructorDemo cd = new ConstructorDemo();
+        ConstructorDemo cd = new ConstructorDemo(30,40); //if we do this then the non parameterized constructor will not be called
+//        ConstructorDemo c = new ConstructorDemo();
     }
 }
