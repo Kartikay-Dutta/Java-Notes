bfe653833ca277a8ff9179a874ecb4c6aeb8c80e
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-02-16 11:41:27 +0530

    info

--- src/OOPS/static_basic.java
+++ src/OOPS/static_basic.java
@@ -2,10 +2,21 @@ package OOPS;
 
 public class static_basic {
     int a = 10;
-    static int b = 20;  // statics lietime is the duration of the programns execution
+    static int b = 20;  // statics lifetime is the duration of the programs execution
 
+    void fun(){
+        a = 20;
+        b = 30;
+    }
     public static void main(String[] args) {
-//        System.out.println(a); doesnt print coz default constructor main chala gya and without object call nahi hoga
+//        System.out.println(a); // print nahi hoga coz it neeeds a object to exist
+        System.out.println(b); // main is static so it can directly access b
+        static_basic sb = new static_basic();
+        System.out.println(sb.a);
+
+        sb.fun(); //updated values print hogi
+        System.out.println(sb.a);
         System.out.println(b);
+
     }
 }
