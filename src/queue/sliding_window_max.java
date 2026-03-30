55afc9bb720f5b631ef321de0a45801fc75073e5
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2026-03-30 12:41:56 +0530

    sliding window

---
+++ src/queue/sliding_window_max.java
@@ -0,0 +1,26 @@
+package queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// time complexity = O(n)
+public class sliding_window_max {
+    public static void main(String[] args) {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i = 0;i<arr.length;i++){
+
+            if(!dq.isEmpty() && dq.peekFirst() == i-k) dq.removeFirst();
+
+            while(!dq.isEmpty() && arr[dq.peekLast()] < arr[i]) dq.removeLast();
+
+            dq.addLast(i);
+
+            if(i > k-1) System.out.print(arr[dq.peekFirst()] + " ");
+        }
+    }
+
+}
+
